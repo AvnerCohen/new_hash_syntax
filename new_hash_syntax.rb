1784f8daf45bd7acf718b2c4da31a1ad3ce34de9
AvnerCohen <israbirding@gmail.com>
2012-10-09 21:37:01 +0200

    Support for full and partial mode

--- new_hash_syntax.rb
+++ new_hash_syntax.rb
@@ -2,15 +2,22 @@
 
 file_name = ARGV[0]
 
-if file_name.isBlank?
-	puts "Syntax: new_hash_syntax.rb file_name.rb [CODE].\nCODE - Add to also change code lines and not only comments"
+if file_name.nil?
+	puts "Syntax: new_hash_syntax.rb file_name.rb <FULL>\nFULL - Add to also change code lines and not only comments"
+	exit 1
 end
+
+full_mode = (ARGV[1].upcase === "FULL")
 
 new_file = ""
 file = File.new(file_name, "r")
+
 while (line = file.gets)
-	
-	new_file+=line.gsub(/:(\w+) =>/, "$1:")
+	if line.lstrip.start_with?("#") or full_mode
+		new_file+= to_new_syntax(line)
+	else
+		new_file+= line
+	end
 
 end
 file.close
@@ -20,3 +27,8 @@ file.write(new_file)
 file.close
 
 puts "Done. Created " + file_name + "_updated"
+
+
+def to_new_syntax(line)
+	line.gsub(/:(\w+) =>/, '\1:')
+end
